417852e04352c18980cce3eef3c53736e3f70e15
Sahil Kumar <143871621+Sahilkumar70@users.noreply.github.com>
2025-12-20 03:21:13 +0530

    Javascriptcommit

---
+++ practise1.js
@@ -0,0 +1,8 @@
+// Print all even numbers from 1 to 100 ?
+for (let i=1; i<=100; i++){
+    if (i%2==0){
+        console.log("i=",i);
+    }else{
+        console.log("not even number");
+    }
+}
---
+++ practise2.js
@@ -0,0 +1,9 @@
+//create a game where you start with any random game no ask the user to keep guessing the gameno until the user enters correct value?
+let no = 30;
+let userNo = prompt("try to guess no:");{
+    while(no != userNo){
+        userNo = prompt("aaah bro tryagain:");
+    }
+}
+console.log("Right guess");
+prompt("bro you are great");
